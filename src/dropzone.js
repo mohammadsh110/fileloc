7b4b8a0ee87f9aa319ea09989729fc7e8c995e90
mohammadsh110 <m.shammasi@gmail.com>
2021-12-04 22:07:31 -0800

    Update dropzone.js

--- src/dropzone.js
+++ src/dropzone.js
@@ -5,7 +5,7 @@ import 'react-dropzone-uploader/dist/styles.css'
 const DropImage = (props) => {
     const [statusDrop, setStat] = useState('Drag File Here and submit');
     const getUploadParams = ({ meta }) => {
-        const url = 'https://filesharelocation.herokuapp.com:3000/uploadImage/'
+        const url = 'https://osanalyzer.herokuapp.com:3000/uploadImage/'
         return { url, meta: { fileUrl: `${url}` } }
     }
     const handleChangeStatus = ({ meta }, status) => {
@@ -16,7 +16,7 @@ const DropImage = (props) => {
         formData.append("lat", props.lat);
         formData.append("lon", props.lon);
         fetch(
-            'https://filesharelocation.herokuapp.com:3000/location/',
+            'https://osanalyzer.herokuapp.com:3000/location/',
             {
                 method: 'POST',
                 body: formData,
@@ -33,7 +33,7 @@ const DropImage = (props) => {
     }
     const handleSubmit = (files, allFiles) => {
         fetch(
-            'https://filesharelocation.herokuapp.com:3000/uploadImage/',
+            'https://osanalyzer.herokuapp.com:3000/uploadImage/',
             {
                 method: 'POST',
                 body: files.map(f => f.meta),
